1848e132c2ec8b9ffc160c1e22b24df81f376321
camille <cerulean.dsgn@gmail.com>
2023-03-06 21:30:19 -0300

    chore: installing first libraries

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+
+
+// server.use([]);
+const PORT = process.env.PORT || 5000;
+
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
