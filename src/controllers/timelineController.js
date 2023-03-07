2a9495808f66e8982d0aa3746bb208ba38bf5cd4
Laura <laurasouzacastro1@gmail.com>
2023-03-07 19:19:46 -0300

    feat: insert table posts_hashtags

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -1,12 +1,11 @@
 import { db } from "../database/database.js";
 
-export async function getInfos(req, res){
-
+export async function getTimeline(req, res){
     try{
         const session = res.locals.session;
         const url = await db.query(`SELECT picture_url FROM users WHERE id=$1;`, [session.user_id])
-
-        return res.send(url.rows[0]);
+        const posts = await db.query(`SELECT posts.id, posts.text, posts.url, users.picture_url, users.username FROM posts JOIN users ON users.id = posts.user_id ORDER BY posts.id DESC LIMIT 20;`)
+        return res.send({avatar: url.rows[0].picture_url, posts: posts.rows});
     } catch (error){
         res.status(500).send(error.message)
     }
@@ -35,6 +34,12 @@ export async function postPublish(req, res){
                         VALUES ($1);
                     `, [element])
                 }
+                const hashtagId = await db.query(`SELECT id FROM hashtags WHERE name=$1;`, [element])
+                const postId = await db.query(`SELECT id from posts WHERE user_id=$1 order by id desc limit 1;`, [session.user_id])
+                await db.query(`
+                        INSERT INTO posts_hashtags (post_id, hashtag_id)
+                        VALUES ($1, $2);
+                    `, [postId.rows[0].id, hashtagId.rows[0].id])
             });
         }
 
--- src/routes/timelineRoutes.js
+++ src/routes/timelineRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { getInfos, postPublish } from "../controllers/timelineController.js";
+import { getTimeline, postPublish } from "../controllers/timelineController.js";
 import { authValidation } from "../middlewares/authValidation.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { postSchema } from "../schemas/postSchema.js";
@@ -7,7 +7,7 @@ import { postSchema } from "../schemas/postSchema.js";
 const timelineRoutes = Router();
 
 timelineRoutes.use(authValidation);
-timelineRoutes.get("/timeline", getInfos);
+timelineRoutes.get("/timeline", getTimeline);
 timelineRoutes.post("/publish", validateSchema(postSchema), postPublish);
 
 export default timelineRoutes;
