0cb7e218339e056c90aa85af95e1a0f599a2df73
geisniwander <geisniwander@gmail.com>
2023-03-09 13:26:14 -0300

    feat: implementing getUserProfile route

--- src/repositories/timelineRepositories.js
+++ src/repositories/timelineRepositories.js
@@ -1,44 +1,91 @@
 import { db } from "../database/database.js";
 
 export async function postPublishRepository(link, description, user_id) {
-    await db.query(`
+  await db.query(
+    `
         INSERT INTO posts (url, text, user_id)
         VALUES ($1, $2, $3);
-    `, [link, description, user_id])
+    `,
+    [link, description, user_id]
+  );
 }
 
 export async function getHashtagsByNameRepository(name) {
-    return await db.query(`SELECT id FROM hashtags WHERE name=$1;`, [name])
+  return await db.query(`SELECT id FROM hashtags WHERE name=$1;`, [name]);
 }
 
 export async function postHashtagsByNameRepository(name) {
-    await db.query(`INSERT INTO hashtags (name) VALUES ($1);`, [name])
+  await db.query(`INSERT INTO hashtags (name) VALUES ($1);`, [name]);
 }
 
 export async function getPostByUserIdRepository(user_id) {
-    return await db.query(`SELECT id from posts WHERE user_id=$1 order by id desc limit 1;`, [user_id])
+  return await db.query(
+    `SELECT id from posts WHERE user_id=$1 order by id desc limit 1;`,
+    [user_id]
+  );
 }
 
 export async function postPostsHashtagsRepository(postId, hashtagId) {
-    await db.query(`INSERT INTO posts_hashtags (post_id, hashtag_id) VALUES ($1, $2);`, [postId, hashtagId])
+  await db.query(
+    `INSERT INTO posts_hashtags (post_id, hashtag_id) VALUES ($1, $2);`,
+    [postId, hashtagId]
+  );
 }
 
 export async function getAvatarByUserIdRepository(user_id) {
-    return await db.query(`SELECT picture_url, username FROM users WHERE id=$1;`, [user_id])
+  return await db.query(
+    `SELECT picture_url, username FROM users WHERE id=$1;`,
+    [user_id]
+  );
 }
 
 export async function getPostsRepository() {
-    return await db.query(`SELECT posts.id, posts.text, posts.url, users.picture_url, users.username FROM posts JOIN users ON users.id = posts.user_id ORDER BY posts.id DESC LIMIT 20;`)
+  return await db.query(
+    `SELECT posts.id, posts.text, posts.url, users.picture_url, users.username FROM posts JOIN users ON users.id = posts.user_id ORDER BY posts.id DESC LIMIT 20;`
+  );
 }
 
 export async function getLikesRepository() {
-    return await db.query(`select users.username, likes.post_id from likes join users on likes.user_id = users.id;`)
+  return await db.query(
+    `select users.username, likes.post_id from likes join users on likes.user_id = users.id;`
+  );
 }
 
 export async function postLikesByPostIdRepository(postId, userId) {
-    await db.query(`INSERT INTO likes (post_id, user_id) VALUES ($1, $2);`, [postId, userId])
+  await db.query(`INSERT INTO likes (post_id, user_id) VALUES ($1, $2);`, [
+    postId,
+    userId,
+  ]);
 }
 
 export async function deleteLikesByPostIdRepository(postId, userId) {
-    await db.query(`delete from likes where post_id = $1 AND user_id = $2;`, [postId, userId])
+  await db.query(`delete from likes where post_id = $1 AND user_id = $2;`, [
+    postId,
+    userId,
+  ]);
+}
+
+export async function getTimelineByUserIdRepository(user_id) {
+  return await db.query(
+    `
+      SELECT json_build_object(
+        'id', users.id,
+        'username', users.username,
+        'picture_url', users.picture_url,
+        'user_posts', json_agg(
+          json_build_object(
+            'id', posts.id,
+            'user_id', posts.user_id,
+            'picture_url', users.picture_url,
+            'text', posts.text,
+            'url', posts.url
+          )
+        )
+      ) AS user
+      FROM users, posts
+      WHERE users.id = $1 AND users.id = posts.user_id
+      GROUP BY users.id;
+    `,
+    [user_id]
+  );
 }
--- src/routes/timelineRoutes.js
+++ src/routes/timelineRoutes.js
@@ -1,5 +1,13 @@
-import {Router} from "express";
-import { getAvatar, getTimeline, postPublish, getLikes, postLikes, deleteLikes } from "../controllers/timelineController.js";
+import { Router } from "express";
+import {
+  getAvatar,
+  getTimeline,
+  postPublish,
+  getLikes,
+  postLikes,
+  deleteLikes,
+  getUserProfile,
+} from "../controllers/timelineController.js";
 import { authValidation } from "../middlewares/authValidation.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { postSchema, likeSchema } from "../schemas/timelineSchema.js";
@@ -10,7 +18,8 @@ timelineRoutes.use(authValidation);
 timelineRoutes.get("/avatar", getAvatar);
 timelineRoutes.get("/timeline", getTimeline);
 timelineRoutes.get("/likes", getLikes);
-timelineRoutes.post("/likes", validateSchema(likeSchema),postLikes);
+timelineRoutes.get("/user/:id", getUserProfile);
+timelineRoutes.post("/likes", validateSchema(likeSchema), postLikes);
 timelineRoutes.delete("/likes/:post_id", deleteLikes);
 timelineRoutes.post("/publish", validateSchema(postSchema), postPublish);
 
